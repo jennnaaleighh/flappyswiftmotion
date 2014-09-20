51443f1d65a4e1432d0b906c5210df011802f611
Dan Stepanov <dhs149@psu.edu>
2014-09-20 13:39:22 -0400

    added flappy swift

---
+++ flappymotion/flappymotion/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  flappymotion
+//
+//  Created by Daniel Stepanov on 9/20/14.
+//  Copyright (c) 2014 unhackathon. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
