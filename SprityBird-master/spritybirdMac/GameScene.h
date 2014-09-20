fcc5d1826347bb2f3fd5e0aa8670e5ae678e9a94
Dan Stepanov <dhs149@psu.edu>
2014-09-20 15:47:58 -0400

    added spritybird

---
+++ SprityBird-master/spritybirdMac/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  spritybirdMac
+//
+
+//  Copyright (c) 2014 Alexis Creuzot. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ SprityBird-master/spritybirdMac/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  spritybirdMac
+//
+//  Created by Daniel Stepanov on 9/20/14.
+//  Copyright (c) 2014 Alexis Creuzot. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
